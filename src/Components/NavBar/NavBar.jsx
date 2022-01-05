4c89a0461443da88dd9361406cb3fbe3a7d43709
ManavellaA <andresraulmanavella@gmail.com>
2022-01-04 21:28:30 -0300

    add

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <div>
+            <h1>Ayelen Galvan DM</h1>
+
+            <ul>
+                <li>Inicio</li>
+                <li>Ofertas</li>
+                <li>Accesorios</li>
+                <li>Ropa de Hombre</li>
+                <li>Ropa de Dama</li>
+            </ul>
+
+        </div>
+    )
+}
