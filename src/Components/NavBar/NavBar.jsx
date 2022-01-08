19b86475df46cad972b1f5fc4666a2236e89ebf9
ManavellaA <andresraulmanavella@gmail.com>
2022-01-08 16:54:33 -0300

    add

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -8,13 +8,13 @@ const NavBar = () => {
         <>
           <a className="d-flex align-items-center justify-content-center m-4 text-decoration-none text-black brand" href="">Ayelen Galvan</a>
 
-          <nav className="navbar navbar-expand-lg navbar-light nav">
-            <div className="container-fluid">
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <nav className="navbar navbar-expand-md navbar-light nav">
+            <div className="container-fluid justify-content-end">
+              <button className="navbar-toggler me-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse align-items-center justify-content-center" id="navbarNav">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav align-items-center justify-content-center">
                   <li className="nav-item">
                     <a className="nav-link itemNav" href="">Inicio</a>
                   </li>
