a06861a29d05da72c34f93b0061089531928fc78
ManavellaA <andresraulmanavella@gmail.com>
2022-01-10 21:33:09 -0300

    add

--- src/Components/Cart/CartIcon.jsx
+++ src/Components/Cart/CartIcon.jsx
@@ -5,7 +5,7 @@ import { BiCart } from 'react-icons/bi';
 
 class CartIcon extends React.Component {
   render() {
-    return <h3 className='ms-3 me-3'><BiCart /></h3>  
+    return <h3 className='ms-3 me-3' type="button"><BiCart /></h3>  
   }
 }
 
