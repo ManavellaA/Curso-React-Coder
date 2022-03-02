4dc4ff17447c874de97b64d449f941179f5ac0cf
ManavellaA <andresraulmanavella@gmail.com>
2022-03-02 00:06:28 -0300

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import CartContext from './Components/Cart/CartContext';
-import NavBar from "./Components/NavBar/NavBar"; 
+import NavBar from "./Components/NavBar/NavBar";
+import Footer from "./Components/Footer/Footer";
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailConteiner';
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
 import BuyForm from "./Components/Cart/BuyForm";
@@ -10,38 +11,40 @@ import CartWidget from './Components/Cart/CartWidget';
 
 function App() {
 
-  const arrayElementsNavBar = [{id:1 , item: 'Inicio', enlace:'/'},
-                               {id:2 , item: 'Ropa de dama', enlace:'/Tienda/Dama'},
-                               {id:3 , item: 'Ropa de hombre', enlace:'/Tienda/Hombre'},
-                               {id:4 , item: 'Accesorios', enlace:'/Tienda/Accesorios'}
-                              ];
+  const arrayElementsNavBar = [
+  { id: 1, item: 'Inicio', enlace: '/' },
+  { id: 2, item: 'Ropa de dama', enlace: '/Tienda/Dama' },
+  { id: 3, item: 'Ropa de hombre', enlace: '/Tienda/Hombre' },
+  { id: 4, item: 'Accesorios', enlace: '/Tienda/Accesorios' }
+  ];
 
   const brand = 'Ayelen Galvan'
 
   return (
     <CartContext>
       <BrowserRouter>
-        <NavBar arrayElementsNavBar={arrayElementsNavBar} brand={brand} /> 
+        <NavBar arrayElementsNavBar={arrayElementsNavBar} brand={brand} />
         <Switch>
 
-            <Route exact path='/'> 
-                <ItemListContainer /> 
-            </Route>
-  
-            <Route exact path='/Tienda/:category'>
-                <ItemListContainer />
-            </Route>
-  
-            <Route exact path='/Tienda/Item/:id'>
-                <ItemDetailContainer />
-            </Route>
-  
-            <Route exact path='/Compra'>
-              <BuyForm />
-            </Route>           
+          <Route exact path='/'>
+            <ItemListContainer />
+          </Route>
+
+          <Route exact path='/Tienda/:category'>
+            <ItemListContainer />
+          </Route>
+
+          <Route exact path='/Tienda/Item/:id'>
+            <ItemDetailContainer />
+          </Route>
+
+          <Route exact path='/Compra'>
+            <BuyForm />
+          </Route>
 
         </Switch>
-      <CartWidget /> 
+        <CartWidget />
+        <Footer />
       </BrowserRouter>
     </CartContext>
   );
